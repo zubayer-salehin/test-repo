ab5b9765d9742e82152d7a9c5dcf7a909e62585a
zubayer-salehin <zubaersalehin@gmail.com>
2024-11-18 23:33:12 +0600

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-unused-vars": "error",
+      "prefer-const": "error",
+      "no-unused-expressions": "error",
+      "no-console": "warn",
+      "no-undef": "error",
+      eqeqeq: "error",
+    },
+  },
+  pluginJs.configs.recommended,
+];
